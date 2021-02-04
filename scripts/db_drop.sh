b30a7e8330a893127543af94c0c6f41638711956
Evgeniy <1053418+spleshka@users.noreply.github.com>
2021-02-04 12:27:17 +0300

    Update db_drop.sh

--- scripts/db_drop.sh
+++ scripts/db_drop.sh
@@ -6,9 +6,13 @@ export MYSQL_PWD="$DB_MASTER_PASSWORD"
 export MYSQL_HOST="$DB_HOST"
 export MYSQL_TCP_PORT="$DB_PORT"
 
-echo "DEBUG: Deleting the user ${DB_USER} if exists..."
-mysql -e "DROP USER IF EXISTS '${DB_USER}'@'%';"
-echo "DEBUG: Done"
+# TODO: Temporary removed user deletion, because for long-name
+# environments the username generated maybe the same (due to trim to 32chars)
+# and removal of one env may delete user with the same name.
+# This needs to be fixed before we uncomment this.
+#echo "DEBUG: Deleting the user ${DB_USER} if exists..."
+#mysql -e "DROP USER IF EXISTS '${DB_USER}'@'%';"
+#echo "DEBUG: Done"
 
 echo "DEBUG: Deleting the database ${DB_NAME} if exists..."
 mysql -e "DROP DATABASE IF EXISTS ${DB_NAME};"
