14da16ae3678fa6819528317215bb5bab9fd2b85
Evgeniy Maslovskiy <spleshka@gmail.com>
2022-11-14 17:05:58 +0100

    Fixed import command.

--- scripts/files_import.sh
+++ scripts/files_import.sh
@@ -4,7 +4,7 @@ set -e
 ### Public files.
 
 echo "DEBUG: Starting import of public files from $FILES_PUBLIC_BACKUPS_S3_FOLDER to $FILES_PUBLIC_FOLDER..."
-time aws s3cmd sync --delete-removed "$FILES_PUBLIC_BACKUPS_S3_FOLDER" "$FILES_PUBLIC_FOLDER"
+time s3cmd sync --recursive --preserve --delete-removed "$FILES_PUBLIC_BACKUPS_S3_FOLDER" "$FILES_PUBLIC_FOLDER"
 echo "DEBUG: Done"
 
 echo "DEBUG: Changing owner of public files to ${OWNER_UID}:${OWNER_GID}..."
@@ -18,7 +18,7 @@ echo "DEBUG: Done"
 ### Private files.
 
 echo "DEBUG: Starting import of private files from $FILES_PRIVATE_BACKUPS_S3_FOLDER to $FILES_PRIVATE_FOLDER..."
-time aws s3cmd sync --delete-removed "$FILES_PRIVATE_BACKUPS_S3_FOLDER" "$FILES_PRIVATE_FOLDER"
+time s3cmd sync --recursive --preserve --delete-removed "$FILES_PRIVATE_BACKUPS_S3_FOLDER" "$FILES_PRIVATE_FOLDER"
 echo "DEBUG: Done"
 
 echo "DEBUG: Changing owner of private files to ${OWNER_UID}:${OWNER_GID}..."
